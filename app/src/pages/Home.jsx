97cb9cf5e603b761ea968a9ae1298917485f85ad
LuvLuvChloe <chloe.tsaoluv@gmail.com>
2024-11-06 03:07:12 +0000

    241104 home page features

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,5 +1,71 @@
-export function Home ( props ) {
+import { useContext, useState, useEffect } from "react"
+import { FirestoreContext } from "../contexts/FirestoreContext"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import { collection, getDocs } from "firebase/firestore"
+
+export function Home(props) {
+    const [bookdata, setBookData] = useState([])
+    let booksLoaded = false
+
+    const db = useContext(FirestoreContext)
+
+    const getBooks = async () => {
+        const booksCollection = collection(db, "Books")
+        const result = await getDocs(booksCollection)
+        let booksArray = []
+        result.forEach((doc) => {
+            let book = doc.data()
+            book.id = doc.id
+            booksArray.push(book)
+
+        })
+        setBookData(booksArray)
+        console.log(booksArray)
+    }
+
+    useEffect(() => {
+        if (booksLoaded == false) {
+            getBooks()
+            booksLoaded = true
+        }
+    }, [booksLoaded])
+
+    const Books = bookdata.map((book) => {
+        return (
+            <Col md={3}>
+                <Card>
+                    <Card.Img 
+                    variant="top" 
+                    src={"/book_covers/" + book.Cover}
+                    style={{maxWidth:"100%"}}
+                    />
+                    <Card.Body>
+                        <Card.Title>{ book.Title }</Card.Title>
+                        <Card.Text>
+                            A book by {book.Author}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        )
+    })
+
     return (
-        <h1>Home</h1>
+        <Container fluid>
+            <Row>
+                <Col>
+                    <h1>Library System</h1>
+                </Col>
+            </Row>
+            <Row>
+                {/* books here */}
+                { Books }
+            </Row>
+        </Container>
+
     )
 }
